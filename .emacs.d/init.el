408276e9e3feda4959e061c33c9b5192c8bcc586
Patrick Young <patrick.young@digitalglobe.com>
2014-05-05 13:42:05 +0000

    New freshness.

---
+++ .emacs.d/init.el
@@ -0,0 +1,49 @@
+
+;; deal with backup files by putting them in /tmp
+(setq backup-directory-alist
+          `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+          `((".*" ,temporary-file-directory t)))
+
+;; enable ido mode
+(require 'ido)
+(ido-mode t)
+
+;; ELPA repos.
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;; el-get configuration
+(setq el-get-github-default-url-type "https")
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)                      ; optional
+(setq jedi:complete-on-dot t)                 ; optional
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rst-level-1 ((t (:background "black"))))
+ '(rst-level-2 ((t (:background "black"))))
+ '(rst-level-3 ((t (:background "black"))))
+ '(rst-level-4 ((t (:background "black"))))
+ '(rst-level-5 ((t (:background "black")))))
